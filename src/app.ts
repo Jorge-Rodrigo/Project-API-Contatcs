1ad2073a335a0736fa5af33eb3c47a7c8c255e32
Jorge Rodrigo <86239864+Jorge-Rodrigo@users.noreply.github.com>
2023-06-01 14:18:59 -0300

    fix: CORS authorization

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,11 @@ import { userRoutes } from "./routes/users.routes";
 import { handleErrors } from "./errors";
 import loginRouter from "./routes/login.routes";
 import { contactsRoutes } from "./routes/contacts.routes";
-
+let cors = require("cors");
 const app: Application = express();
+
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", userRoutes);
 app.use("/login", loginRouter);
